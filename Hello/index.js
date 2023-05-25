3b0572d235e365ca3f4b975d5bc9b7d574d3dd9b
wny2023 <wiber4001@gmail.com>
2023-05-25 15:12:19 +0900

    RNDay01수업

--- Hello/index.js
+++ Hello/index.js
@@ -8,3 +8,5 @@ import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 // 화면에 들어갈 component를 설정함, 객체설정없이 쓰므로 이것은 static임
+//2번째 파라미터에서 App을 리턴함
+// appName이라는 화면에 App함수를 리턴하는 명령
